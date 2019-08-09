63539fa6619eadd6f084ecbb20bd5a999dbf3a8d
hui.liu <hui.liu@bianlifeng.com>
2019-08-09 14:37:00 +0800

    增加eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: 'eslint-config-rnx',
+  globals: {
+    __DEV__: false,
+    B: false,
+    window: false,
+    document: false,
+    fetch: false,
+  },
+  rules: {
+    'jsx-a11y/no-static-element-interactions': ['off'],
+    'react/jsx-no-target-blank': ['off']
+  }
+}
